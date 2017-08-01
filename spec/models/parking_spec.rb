089ffd74e66cce3cfa3e740077f0a45bd55f5170
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-01 22:42:57 +0800

    add test 30min==

---
+++ spec/models/parking_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Parking, type: :model do
+  describe ".calculate_amount" do
+    it "30 mins should be ¥2" do
+      t = Time.now
+      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 30.minutes )
+      parking.calculate_amount
+      expect(parking.amount).to eq(200)
+    end
+  end
+
+  describe ".validate_end_at_with_amount" do
+
+      it "is invalid without amount" do
+        parking = Parking.new( :parking_type => "guest",
+                               :start_at => Time.now - 6.hours,
+                               :end_at => Time.now)
+        expect( parking ).to_not be_valid
+      end
+
+      it "is invalid without end_at" do
+        parking = Parking.new( :parking_type => "guest",
+                               :start_at => Time.now - 6.hours,
+                               :amount => 999)
+        expect( parking ).to_not be_valid
+      end
+    end
+end
