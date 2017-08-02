0ad97694dfc11af5de5bea730205e40b2fa145af
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-02 14:38:33 +0800

    add parking2

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem "devise"
+
 group :development, :test do
   gem 'rspec-rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/models/parking_spec.rb
+++ spec/models/parking_spec.rb
@@ -1,57 +1,115 @@
 require 'rails_helper'
 
 RSpec.describe Parking, type: :model do
+
   describe ".calculate_amount" do
-    it "120 mins should be ¥4" do
-      t = Time.now
-      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 120.minutes )
-      parking.calculate_amount
-      expect( parking.amount ).to eq(400)
+
+    context "guest" do
+            it "120 mins should be ¥4" do
+              t = Time.now
+              parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 120.minutes )
+              parking.calculate_amount
+              expect( parking.amount ).to eq(400)
+            end
+
+            it "90 mins should be ¥3" do
+              t = Time.now
+              parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 90.minutes )
+              parking.calculate_amount
+              expect( parking.amount ).to eq(300)
+            end
+
+            it "61 mins should be ¥3" do
+              t = Time.now
+              parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 61.minutes )
+              parking.calculate_amount
+              expect( parking.amount ).to eq(300)
+            end
+
+            it "60 mins should be ¥2" do
+              t = Time.now
+              parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 60.minutes )
+              parking.calculate_amount
+              expect( parking.amount ).to eq(200)
+            end
+
+            it "30 mins should be ¥2" do
+              t = Time.now
+              parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 30.minutes )
+              parking.calculate_amount
+              expect(parking.amount).to eq(200)
+            end
     end
 
-    it "90 mins should be ¥3" do
-      t = Time.now
-      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 90.minutes )
-      parking.calculate_amount
-      expect( parking.amount ).to eq(300)
-    end
+    context "short-term" do
+            it "30 mins should be ¥2" do
+             t = Time.now
+             parking = Parking.new( :parking_type => "short-term",
+                                    :start_at => t, :end_at => t + 30.minutes )
+             parking.user = User.create(:email => "test@example.com", :password => "123455678")
 
-    it "61 mins should be ¥3" do
-      t = Time.now
-      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 61.minutes )
-      parking.calculate_amount
-      expect( parking.amount ).to eq(300)
-    end
+             parking.calculate_amount
+             expect(parking.amount).to eq(200)
+            end
 
-    it "60 mins should be ¥2" do
-      t = Time.now
-      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 60.minutes )
-      parking.calculate_amount
-      expect( parking.amount ).to eq(200)
-    end
+            it "60 mins should be ¥2" do
+             t = Time.now
+             parking = Parking.new( :parking_type => "short-term",
+                                    :start_at => t, :end_at => t + 60.minutes )
+             parking.user = User.create(:email => "test@example.com", :password => "123455678")
 
-    it "30 mins should be ¥2" do
-      t = Time.now
-      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 30.minutes )
-      parking.calculate_amount
-      expect(parking.amount).to eq(200)
+             parking.calculate_amount
+             expect( parking.amount ).to eq(200)
+            end
+
+            it "61 mins should be ¥2.5" do
+             t = Time.now
+             parking = Parking.new( :parking_type => "short-term",
+                                    :start_at => t, :end_at => t + 61.minutes )
+             parking.user = User.create(:email => "test@example.com", :password => "123455678")
+
+             parking.calculate_amount
+
+             expect( parking.amount ).to eq(250)
+            end
+
+            it "90 mins should be ¥2.5" do
+             t = Time.now
+             parking = Parking.new( :parking_type => "short-term",
+                                    :start_at => t, :end_at => t + 90.minutes )
+             parking.user = User.create(:email => "test@example.com", :password => "123455678")
+
+             parking.calculate_amount
+             expect( parking.amount ).to eq(250)
+            end
+
+            it "120 mins should be ¥3" do
+             t = Time.now
+             parking = Parking.new( :parking_type => "short-term",
+                                    :start_at => t, :end_at => t + 120.minutes )
+             parking.user = User.create(:email => "test@example.com", :password => "123455678")
+
+             parking.calculate_amount
+             expect( parking.amount ).to eq(300)
+            end
+
     end
   end
 
   describe ".validate_end_at_with_amount" do
 
-      it "is invalid without amount" do
-        parking = Parking.new( :parking_type => "guest",
-                               :start_at => Time.now - 6.hours,
-                               :end_at => Time.now)
-        expect( parking ).to_not be_valid
-      end
+            it "is invalid without amount" do
+              parking = Parking.new( :parking_type => "guest",
+                                     :start_at => Time.now - 6.hours,
+                                     :end_at => Time.now)
+              expect( parking ).to_not be_valid
+            end
 
-      it "is invalid without end_at" do
-        parking = Parking.new( :parking_type => "guest",
-                               :start_at => Time.now - 6.hours,
-                               :amount => 999)
-        expect( parking ).to_not be_valid
-      end
+            it "is invalid without end_at" do
+              parking = Parking.new( :parking_type => "guest",
+                                     :start_at => Time.now - 6.hours,
+                                     :amount => 999)
+              expect( parking ).to_not be_valid
+            end
     end
 end
