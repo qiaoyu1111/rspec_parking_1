06b36699fe6024fbb2816b1d6b327d2676784c1b
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-02 20:18:04 +0800

    add

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -40,13 +40,40 @@ class Parking < ApplicationRecord
   #  end
 
   # 重构上面👆那段代码
-    def calculate_amount
-      if self.amount.blank? && self.start_at.present? && self.end_at.present?
-        if duration <= 60
-          self.amount = 200
-        else
-          self.amount = 200 + ((duration - 60).to_f / 30).ceil * 100
-        end
+  def calculate_amount
+
+    puts "----"
+    puts self.parking_type
+    puts "----"
+
+    if self.amount.blank? && self.start_at.present? && self.end_at.present?
+      if self.user.blank?
+        self.amount = calculate_guest_term_amount  # 一般费率
+      elsif self.parking_type == "long-term"
+          self.amount = calculate_long_term_amount # 长期费率
+      elsif self.parking_type == "short-term"
+        self.amount = calculate_short_term_amount  # 短期费率
       end
     end
+  end
+
+  def calculate_guest_term_amount
+    if duration <= 60
+      self.amount = 200
+    else
+      self.amount = 200 + ((duration - 60).to_f / 30).ceil * 100
+    end
+  end
+
+  def calculate_short_term_amount
+    if duration <= 60
+      self.amount = 200
+    else
+      self.amount = 200 + ((duration - 60).to_f / 30).ceil * 50
+    end
+  end
+
+  def calculate_long_term_amount
+    # TODO
+  end
 end
